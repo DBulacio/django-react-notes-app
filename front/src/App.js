ccb688f364e76d4304fc584220e83b9478da59a8
DBulacio <daniel.bulacio@bodyhealthgroup.com>
2023-12-04 15:38:36 -0300

    adding some styling

--- front/src/App.js
+++ front/src/App.js
@@ -11,12 +11,14 @@ import NotePage from './pages/NotePage'
 const App = () => {
   return (
     <Router>
-      <div className='App'>
-        <Header />
-        <Routes>
-          <Route path="/" exact element={<NotesListPage />} />
-          <Route path="/note/:id" element={<NotePage />} />
-        </Routes>
+      <div className='container dark'>
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<NotesListPage />} />
+            <Route path="/note/:id" element={<NotePage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   )
--- front/src/pages/NotePage.js
+++ front/src/pages/NotePage.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
+import { Link } from 'react-router-dom'
 
 const NotePage = () => {
   const { id } = useParams()
@@ -17,8 +19,15 @@ const NotePage = () => {
   }
 
   return (
-    <div>
-      <h1>{note?.body}</h1>
+    <div className='note'>
+      <div className='note-header'>
+        <h3>
+          <Link to="/">
+            <ArrowLeft />
+          </Link>
+        </h3>
+      </div>
+      <textarea defaultValue={note?.body}></textarea>
     </div>
   )
 }
